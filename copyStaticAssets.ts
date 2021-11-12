76284e0b205fa397e982ad0abea869afb1333c90
Djibril <issoufoudjib@gmail.com>
2021-11-12 16:00:47 +0300

    copy statics assets after build

---
+++ copyStaticAssets.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-console */
+import ncp from 'ncp';
+import { promisify } from 'util';
+
+const copy = promisify(ncp);
+
+async function copyFiles(source, destination) {
+  return copy(source, destination);
+}
+
+copyFiles('.env','./dist/.env');

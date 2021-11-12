1dac7a09c9de748f1a2db0c9adca03262ae03e7c
Djibril <issoufoudjib@gmail.com>
2021-11-12 16:44:40 +0300

    logger with winston instead of console.log

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
+import { logger } from './config';
 
 const app = express();
 app.listen(4000, () => {
-  console.log('🚀 server listening on port 4000');
+  logger.info('🚀 server listening on port 4000');
 });
